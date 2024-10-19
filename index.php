65eb20a2045cf15171ecc15436d6e569708aaa9d
jhonmoreno1992 <2520221010@coreducacion.edu.co>
2024-10-19 12:23:47 -0500

    Primer Avance por git

---
+++ index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <title>DETALLES CON DULZURA</title>
+</head>
+
+<body>
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container px-4 px-lg-5">
+            <a class="navbar-brand" href="#!">Bienvenidos</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Principal</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#!">Nosotros</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Categorias</a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#!">Amor</a></li>
+                            <li><a class="dropdown-item" href="#!">Amistad</a></li>
+                            <li><a class="dropdown-item" href="#!">Cumpleaños</a></li>
+                            <li><a class="dropdown-item" href="#!">Temporadas</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <button class="btn btn-outline-dark" type="submit">
+                        <i class="bi-cart-fill me-1"></i>
+                        Login
+                        <span class="badge bg-dark text-white ms-1 rounded-pill"> </span>
+                    </button>
+                    <button class="btn btn-outline-dark" type="submit">
+                        <i class="bi-cart-fill me-1"></i>
+                        Carrito
+                        <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                    </button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <!-- Header-->
+    <header class="bg-dark py-5" style="background:url('asset/imagen2.jpg')">
+        <div class="container px-4 px-lg-5 my-5">
+            <div class="text-center text-black">
+                <h1 class="display-4 fw-bolder">DETALLES CON DULZURA</h1>
+                <p class="lead fw-normal text-black">Detalles para todas las ocasiones</p>
+            </div>
+        </div>
+    </header>
+    <!-- Section-->
+    <section class="py-5">
+        <div class="container px-4 px-lg-5 mt-5">
+            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                <div class="col mb-5">
+                    <div class="card h-100">
+                        <!-- Product image-->
+                        <img class="card-img-top" src="asset/producto1.jpg" alt="..." />
+                        <!-- Product details-->
+                        <div class="card-body p-4 lila-background">
+                            <div class="text-center">
+                                <!-- Product name-->
+                                <h5 class="fw-bolder">Peluches</h5>
+                                <!-- Product price-->
+                                $15.000 - $50.000
+                            </div>
+                        </div>
+                        <!-- Product actions-->
+                        <div class="card-footer p-4 pt-0 border-top-0 lila-background">
+                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Ver Opciones</a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-5">
+                    <div class="card h-100">
+                        <!-- Product image-->
+                        <img class="card-img-top" src="asset/producto2.jpg" alt="..." />
+                        <!-- Product details-->
+                        <div class="card-body p-4 lila-background">
+                            <div class="text-center">
+                                <!-- Product name-->
+                                <h5 class="fw-bolder">Flores</h5>
+                                <!-- Product price-->
+                                $4.000 - $50.000
+                            </div>
+                        </div>
+                        <!-- Product actions-->
+                        <div class="card-footer p-4 pt-0 border-top-0 lila-background">
+                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Ver Opciones</a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-5">
+                    <div class="card h-100">
+                        <!-- Sale badge-->
+                        <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Oferta</div>
+                        <!-- Product image-->
+                        <img class="card-img-top" src="asset/producto3.jpg" alt="..." />
+                        <!-- Product details-->
+                        <div class="card-body p-4 lila-background">
+                            <div class="text-center">
+                                <!-- Product name-->
+                                <h5 class="fw-bolder">Chocolates</h5>
+                                <!-- Product reviews-->
+                                <div class="d-flex justify-content-center small text-warning mb-2">
+                                    <div class="bi-star-fill"></div>
+                                    <div class="bi-star-fill"></div>
+                                    <div class="bi-star-fill"></div>
+                                    <div class="bi-star-fill"></div>
+                                    <div class="bi-star-fill"></div>
+                                </div>
+                                <!-- Product price-->
+                                <span class="text-muted text-decoration-line-through">$10.000</span>
+                                $8.000
+                            </div>
+                        </div>
+                        <!-- Product actions-->
+                        <div class="card-footer p-4 pt-0 border-top-0 lila-background">
+                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Ver Opciones</a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-5">
+                    <div class="card h-100">
+                        <!-- Sale badge-->
+                        <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Oferta</div>
+                        <!-- Product image-->
+                        <img class="card-img-top" src="asset/ancheta.jpeg" alt="..." />
+                        <!-- Product details-->
+                        <div class="card-body p-4 lila-background">
+                            <div class="text-center">
+                                <!-- Product name-->
+                                <h5 class="fw-bolder">Anchetas de Temporada</h5>
+                                <!-- Product price-->
+                                <span class="text-muted text-decoration-line-through">$50.000</span>
+                                $45.000
+                            </div>
+                        </div>
+                        <!-- Product actions-->
+                        <div class="card-footer p-4 pt-0 border-top-0 lila-background">
+                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Ver Opciones</a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-5">
+                    <div class="card h-100">
+                        <!-- Product image-->
+                        <img class="card-img-top" src="asset/peluche.jpeg" alt="..." />
+                        <!-- Product details-->
+                        <div class="card-body p-4 lila-background">
+                            <div class="text-center">
+                                <!-- Product name-->
+                                <h5 class="fw-bolder">Destacado</h5>
+                                <h6>Peluche Charmander</h6>
+                                <!-- Product reviews-->
+                                <div class="d-flex justify-content-center small text-warning mb-2">
+                                    <div class="bi-star-fill"></div>
+                                    <div class="bi-star-fill"></div>
+                                    <div class="bi-star-fill"></div>
+                                    <div class="bi-star-fill"></div>
+                                    <div class="bi-star-fill"></div>
+                                </div>
+                                <!-- Product price-->
+                                $25.000
+                            </div>
+                        </div>
+                        <!-- Product actions-->
+                        <div class="card-footer p-4 pt-0 border-top-0 lila-background">
+                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Añadir</a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-5">
+                    <div class="card h-100">
+                        <!-- Sale badge-->
+                        <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Oferta</div>
+                        <!-- Product image-->
+                        <img class="card-img-top" src="asset/rosas.jpg" alt="Caja de Rosas" />
+                        <!-- Product details-->
+                        <div class="card-body p-4 lila-background"> <!-- Fondo lila en los detalles del producto -->
+                            <div class="text-center">
+                                <!-- Product name-->
+                                <h5 class="fw-bolder">Caja de Rosas</h5>
+                                <!-- Product price-->
+                                <span class="text-muted text-decoration-line-through">$30.000</span>
+                                $25.000
+                            </div>
+                        </div>
+                        <!-- Product actions-->
+                        <div class="card-footer p-4 pt-0 border-top-0 lila-background"> <!-- Fondo lila en el pie de la tarjeta -->
+                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Añadir</a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-5">
+                    <div class="card h-100">
+                        <!-- Sale badge-->
+                        <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Oferta</div>
+                        <!-- Product image-->
+                        <img class="card-img-top" src="asset/combo.webp" alt="..." />
+                        <!-- Product details-->
+                        <div class="card-body p-4 lila-background">
+                            <div class="text-center">
+                                <!-- Product name-->
+                                <h5 class="fw-bolder">Combo</h5>
+                                <h6>Vino/Chocolates/Rosas</h6>
+                                <!-- Product reviews-->
+                                <div class="d-flex justify-content-center small text-warning mb-2">
+                                    <div class="bi-star-fill"></div>
+                                    <div class="bi-star-fill"></div>
+                                    <div class="bi-star-fill"></div>
+                                    <div class="bi-star-fill"></div>
+                                    <div class="bi-star-fill"></div>
+                                </div>
+                                <!-- Product price-->
+                                <span class="text-muted text-decoration-line-through">$80.000</span>
+                                $70.000
+                            </div>
+                        </div>
+                        <!-- Product actions-->
+                        <div class="card-footer p-4 pt-0 border-top-0 lila-background">
+                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Añadir</a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-5">
+                    <div class="card h-100">
+                        <!-- Product image-->
+                        <img class="card-img-top" src="asset/desayuno sorpresa.jpg" alt="..." />
+                        <!-- Product details-->
+                        <div class="card-body p-4 lila-background">
+                            <div class="text-center">
+                                <!-- Product name-->
+                                <h5 class="fw-bolder">Destacado</h5>
+                                <h6>Desayuno Sorpresa Princesa</h6>
+                                <!-- Product reviews-->
+                                <div class="d-flex justify-content-center small text-warning mb-2">
+                                    <div class="bi-star-fill"></div>
+                                    <div class="bi-star-fill"></div>
+                                    <div class="bi-star-fill"></div>
+                                    <div class="bi-star-fill"></div>
+                                    <div class="bi-star-fill"></div>
+                                </div>
+                                <!-- Product price-->
+                                $30.000
+                            </div>
+                        </div>
+                        <!-- Product actions-->
+                        <div class="card-footer p-4 pt-0 border-top-0 lila-background">
+                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Añadir</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Footer-->
+    <footer class="py-5" style="background:url('asset/imagen2.jpg')">
+        <div class="container">
+            <p class="m-0 text-center text-black">Copyright &copy; Detalles con Dulzura 2024</p>
+        </div>
+    </footer>
+</body>
+
+</html>
